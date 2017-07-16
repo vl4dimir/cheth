8c6329fb12f10502292f700bb683940629304987
Ivan <ivan.stanojevic@codesynapses.com>
2017-07-16 15:21:10 +0200

    add register and send msg

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,40 @@
+// ReactDOM.render(React.createElement(
+//     'h1',
+//     null,
+//     'Hello, world!'
+// ), document.getElementById('root'));
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
     console.log(Chat);
 
-    Chat.sendMessage("Hello world!");
+    Chat.register("Steamrunner");
 
 
-    Chat.SendMessage().then(function(event) { 
+
+    // Events
+
+    Chat.UserRegister().then(function(event) {
+
+        console.log("New user: ", event.args.nickname);
+        Chat.sendMessage("Hello world!");
+    });
+
+
+    Chat.NotRegistered().then(function(event) {
+
+        if(event.args.from == web3.eth.accounts[0]) {
+            console.log("Sorry your nickname is empty. Please register!");
+            console.log(event)
+        } else {
+            console.log("this isn't message for you, ignore...");
+        }
+
+    });
+
+    Chat.SendMessage().then(function(event) {
         console.log("Message");
-        console.log(event) 
+        console.log(event.args.message)
     });
 });
