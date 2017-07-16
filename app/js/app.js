5072be1e7223d63b0e85bded9d13dd2fc278f928
Ivan <ivan.stanojevic@codesynapses.com>
2017-07-16 14:46:16 +0200

    eve ti event handling

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,12 @@
-Chat.sendMessage("lalalal", "Hello world!");
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    console.log(Chat);
+
+    Chat.sendMessage("Hello world!");
 
 
-Chat.SendMessage().then(function(event) { console.log(event) });
+    Chat.SendMessage().then(function(event) { 
+        console.log("Message");
+        console.log(event) 
+    });
+});
